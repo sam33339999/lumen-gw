f25d64834ac29b8d7b51e6b0a86898945a6ae0ab
sam.zheng <sam33339999@gmail.com>
2022-01-03 23:40:37 +0800

    router add

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+// /bootstrap/app.php
 /** @var \Laravel\Lumen\Routing\Router $router */
 
 /*
@@ -13,6 +13,44 @@
 |
 */
 
+$logStr =<<<LOG
+[{{ method }}]|{{ url }}|{{ requestJson }}|{{ headers }}||
+LOG;
+
+
 $router->get('/', function () use ($router) {
     return $router->app->version();
+});
+
+//https://stackoverflow.com/questions/44711642/lumen-5-4-log-info-level-to-separate-file
+
+$router->get('/hook/{channelId:\d{10}}', function ($channelId, \Illuminate\Http\Request $request) use ($logStr) {
+    /** @var \Illuminate\Log\Logger $logger */
+    $logger = app()->make(\Illuminate\Log\Logger::class);
+    $info = str_replace(['{{ method }}', '{{ url }}', '{{ requestJson }}', '{{ headers }}'],
+        ['GET', $request->fullUrl(), json_encode($request->toArray()), json_encode($request->headers)],
+        $logStr
+    );
+
+    $logger->info($info);
+
+    return response()->json([
+        'success' => 'ok'
+    ]);
+});
+
+
+$router->post('/hook/{channelId:\d{10}}', function ($channelId, \Illuminate\Http\Request $request) use ($logStr) {
+    /** @var \Illuminate\Log\Logger $logger */
+    $logger = app()->make(\Illuminate\Log\Logger::class);
+    $info = str_replace(['{{ method }}', '{{ url }}', '{{ requestJson }}', '{{ headers }}'],
+        ['POST', $request->fullUrl(), json_encode($request->toArray()), json_encode($request->headers)],
+        $logStr
+    );
+
+    $logger->info($info);
+
+    return response()->json([
+        'success' => 'ok'
+    ]);
 });
